b2c3b7e55d6990ae0e5ded5602d0ca9b14e75dc5
Stellar-Stellar <86974310+Stellar-Stellar@users.noreply.github.com>
2024-05-14 13:27:35 -0500

    Set up all files and added object templates

---
+++ scripts.js
@@ -0,0 +1,23 @@
+const gameBoard = (function (){
+    let board = [];
+})();
+
+const gameRules =(function (){
+
+})();
+
+function getPlayer(name) {
+    
+}
+
+//Three objects: game, gameboard, players. Make sure to put each bit of logic in an appropriate spot.
+
+
+
+
+
+
+
+
+
+
